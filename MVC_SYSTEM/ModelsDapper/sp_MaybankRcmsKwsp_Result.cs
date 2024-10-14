467703eb8db81233acd278ed93d65daf27eda9b1
opmsfgvprodata <opms.fgvprodata@gmail.com>
2024-10-14 08:25:22 +0800

    initial sp_MaybankRcmsKwsp_Result.cs

---
+++ MVC_SYSTEM/ModelsDapper/sp_MaybankRcmsKwsp_Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_SYSTEM.ModelsDapper
+{
+    public class sp_MaybankRcmsKwsp_Result
+	{
+        public int fld_ID  { get; set; }
+		public string fld_Nopkj { get; set; }
+		public string fld_Nokp { get; set; }
+		public string fld_Nama { get; set; }
+		public string fld_Kdrkyt { get; set; }
+		public string fld_Kdaktf { get; set; }
+		public string fld_Ktgpkj { get; set; }
+		public string fld_Jenispekerja { get; set; }
+		public string fld_KodSocso { get; set; }
+		public string fld_Noperkeso { get; set; }
+		public string fld_KodKWSP { get; set; }
+		public string fld_Nokwsp { get; set; }
+		public int NegaraID { get; set; }
+		public int SyarikatID { get; set; }
+		public int fld_WilayahID { get; set; }
+		public int fld_LadangID { get; set; }
+		public int fld_DivisionID { get; set; }
+		public string fld_StatusKwspSocso { get; set; }
+		public decimal fld_KWSPPkj { get; set; }
+		public decimal fld_KWSPMjk { get; set; }
+		public decimal fld_SocsoPkj { get; set; }
+		public decimal fld_SocsoMjk { get; set; }
+		public decimal fld_GajiKasar { get; set; }
+		public decimal fld_GajiBersih { get; set; }
+		public int fld_Year { get; set; }
+		public int fld_Month { get; set; }
+	}
